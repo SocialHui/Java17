cf8f24dbac4c2fa219fadc0d8b2c96e4be11f93b
Social Hui <2933347352@qq.com>
2021-02-10 12:11:32 +0800

    单链表，无头结点，第二次提交

---
+++ 链表/单链表/TestDemo.java
@@ -0,0 +1,29 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.createdList();
+        myLinkedList.display();
+        Node node = myLinkedList.findLastNode();
+        System.out.println(node.val);
+        node = myLinkedList.findLastTwoNode();
+        System.out.println(node.val);
+        System.out.println(myLinkedList.size());
+        node = myLinkedList.findN(4);
+        System.out.println(node.val);
+        System.out.println(myLinkedList.contains(100));
+        myLinkedList.addFirst(100);
+        myLinkedList.addFirst(200);
+        myLinkedList.addFirst(300);
+        myLinkedList.display();
+        myLinkedList.addLast(1);
+        myLinkedList.addLast(2);
+        myLinkedList.addLast(3);
+        myLinkedList.display();
+        myLinkedList.addN(2,888);
+        myLinkedList.display();
+        myLinkedList.addLast(888);
+        myLinkedList.display();
+        myLinkedList.removeAllKey(888);
+        myLinkedList.display();
+    }
+}
