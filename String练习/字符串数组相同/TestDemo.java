ffddfb028922c2e900e4a7d531f5fb646519936e
Social Hui <2933347352@qq.com>
2021-02-20 11:21:57 +0800

    判断两个字符串数组是否相同

---
+++ String练习/字符串数组相同/TestDemo.java
@@ -0,0 +1,8 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] str1 = {"ab","c"};
+        String[] str2 = {"a","bc"};
+        System.out.println(solution.arrayStringsAreEqual(str1, str2));
+    }
+}
