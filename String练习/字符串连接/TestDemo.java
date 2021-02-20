21f7214655b4bab33fe89dcc75a36cfbba9f981f
Social Hui <2933347352@qq.com>
2021-02-20 16:07:33 +0800

    字符串连接，不使用库函数。

---
+++ String练习/字符串连接/TestDemo.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str1 = scan.next();
+        String str2 = scan.next();
+        String str = str1 + str2;
+        System.out.println(str);
+    }
+}
