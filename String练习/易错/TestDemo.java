8edff859cfba06b46331aafdb0a477cceb2ad744
Social Hui <2933347352@qq.com>
2021-02-20 17:53:48 +0800

    20210119作业第一题。

---
+++ String练习/易错/TestDemo.java
@@ -0,0 +1,19 @@
+
+public class TestDemo {
+
+    String str = new String("good");
+    char[ ] ch = { 'a' , 'b' , 'c' };
+
+    public static void main(String args[]){
+        TestDemo ex = new TestDemo();
+        ex.change(ex.str,ex.ch);
+        System.out.print(ex.str + " and ");
+        System.out.print(ex.ch);
+    }
+
+    public void change(String str,char ch[ ]){
+        str = "test ok";
+        ch[0] = 'g';
+    }
+
+}
