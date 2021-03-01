7bc87026ef6073b1c160d8a8a6b5d240cc7e4b58
Social Hui <2933347352@qq.com>
2021-03-01 16:46:40 +0800

    后序遍历，第一次提交

---
+++ 二叉树/后序遍历/Solution.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+    List<Integer> list = new LinkedList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
