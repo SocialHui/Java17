9428667cd2b02e44ffb17ae9d488f1df4cb7215f
Social Hui <2933347352@qq.com>
2021-03-01 20:57:52 +0800

    将数组分成相等的三部分，第一次提交

---
+++ 将数组分成相等的三部分/Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        if (sum % 3 != 0) {
+            return false;
+        }
+        int num = sum/3;
+        int num1 = 0;
+        int i = 0;
+        for (; i < arr.length; i++) {
+            num1 += arr[i];
+            if (num1 == num) {
+                break;
+            }
+        }
+        int num2 = 0;
+        int j  = arr.length-1;
+        for (; j > 0; j--) {
+            num2 += arr[j];
+            if (num2 == num) {
+                break;
+            }
+        }
+        if (i >= j || i+1 == j) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ 将数组分成相等的三部分/TestDemo.java
@@ -0,0 +1,7 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {1,3,5,4,6,9,8,2,4,5};
+        System.out.println(solution.canThreePartsEqualSum(arr));
+    }
+}
