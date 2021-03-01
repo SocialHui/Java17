289b5557e05b02134c2a23576fa8642e75093cf7
Social Hui <2933347352@qq.com>
2021-03-01 17:43:17 +0800

    二叉树高度

---
+++ 二叉树/二叉树高度/Solution.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return leftDepth > rightDepth ? leftDepth+1 : rightDepth + 1;
+    }
+}
