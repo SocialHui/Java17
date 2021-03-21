1f1884fac468fae7373f5935d04725bdb6058bdd
ShengHui <2933347352@qq.com>
2021-03-21 17:35:14 +0800

    二叉树前序遍历，非递归

---
+++ 二叉树/二叉树的前序遍历/非递归版本/Solution.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
+    public TreeNode createTree() {
+        TreeNode head = new TreeNode(1);
+        TreeNode node = new TreeNode(2);
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(6);
+        head.left = node;
+        head.right = node1;
+        node.left = node2;
+        node.right = node3;
+        node1.right = node4;
+        return head;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
