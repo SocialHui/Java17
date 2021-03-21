c37df5c8d0e06f34c9bd92cdf3d2b9496d665a08
ShengHui <2933347352@qq.com>
2021-03-21 11:09:02 +0800

    第二次提交

---
+++ 二叉树/二叉树的最大深度/2/Solution.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left)+1;
+        int right = maxDepth(root.right)+1;
+        return left > right ? left : right;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
