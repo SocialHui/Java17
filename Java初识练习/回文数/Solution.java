ceb3ab83176faac8a2a6e4ad775fbbef63e6029f
ShengHui <2933347352@qq.com>
2021-03-25 22:03:02 +0800

    回文数

---
+++ Java初识练习/回文数/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
