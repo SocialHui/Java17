77e8850f842b2b1d467d009409ab6cc529baaae5
ShengHui <2933347352@qq.com>
2021-04-08 18:44:21 +0800

    旋转数组查找最小值，二分法

---
+++ 旋转数组查找/旋转数组.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Solution {
+    public int minNumberInRotateArray(int [] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = array.length-1;
+        int mid = -1;
+        while (array[left] >= array[right]) {
+            if (right - left == 1) {
+                mid = right;
+                break;
+            }
+            mid = (left + right)/2;
+            if (array[mid] >= array[left]) {
+                left = mid;
+            }
+            if (array[mid] <= array[right]) {
+                right = mid;
+            }
+        }
+        return array[mid];
+    }
+}
