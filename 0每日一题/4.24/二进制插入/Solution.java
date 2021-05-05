34932a075a24be18c7819309f85691d61fc314b6
ShengHui <2933347352@qq.com>
2021-05-05 10:22:54 +0800

    二进制插入，位运算

---
+++ 0每日一题/4.24/二进制插入/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution{
+import java.util.*;
+
+public class BinInsert {
+    public int binInsert(int n, int m, int j, int i) {
+        // write code here
+        return n|(m<<j);
+    }
+}
