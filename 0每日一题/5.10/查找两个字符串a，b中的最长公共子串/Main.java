1451f90ea97f1725c381a521306b8dbb796235b2
ShengHui <2933347352@qq.com>
2021-05-10 16:06:56 +0800

    第一次提交，笨方法

--- 0每日一题/5.10/查找两个字符串a，b中的最长公共子串/Main.java
+++ 0每日一题/5.10/查找两个字符串a，b中的最长公共子串/Main.java
@@ -6,17 +6,22 @@ public class Main{
         while (sc.hasNext()) {
             String s1 = sc.nextLine();
             String s2 = sc.nextLine();
-            StringBuilder str = new StringBuilder();   //存放最终需要输出的字符串
-
+            
+            //注意题目说的输出较短的第一次出现的
+            if (s1.length() > s2.length()) {
+                String sm = s1;
+                s1 = s2;
+                s2 = sm;
+            }
+            StringBuilder str = new StringBuilder();
             for (int i = 0; i < s1.length(); i++) {
-
+                
                 for (int j = 0; j < s2.length(); j++) {
-
                     if (s1.charAt(i) == s2.charAt(j)) {
                         StringBuilder s = new StringBuilder();
                         int s1L = i;
                         int s2L = j;
-
+                        
                         while (s1L < s1.length() && s2L < s2.length()) {
                             if (s1.charAt(s1L) == s2.charAt(s2L)) {
                                 s.append(s1.charAt(s1L));
@@ -29,17 +34,14 @@ public class Main{
                         if (s.length() > str.length()) {
                             str = s;
                         }
-
+                        
                     }
-
                 }
-
+                
             }
-
             if (str.length() != 0) {
                 System.out.println(str.toString());
             }
-
         }
     }
 }
