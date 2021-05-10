40b9e17f7a4a3686677e4da5adaa24381662d00a
ShengHui <2933347352@qq.com>
2021-05-10 16:11:13 +0800

    第一次提交，笨方法

--- 0每日一题/5.10/查找两个字符串a，b中的最长公共子串/Main.java
+++ 0每日一题/5.10/查找两个字符串a，b中的最长公共子串/Main.java
@@ -6,22 +6,28 @@ public class Main{
         while (sc.hasNext()) {
             String s1 = sc.nextLine();
             String s2 = sc.nextLine();
-            
+
             //注意题目说的输出较短的第一次出现的
             if (s1.length() > s2.length()) {
                 String sm = s1;
                 s1 = s2;
                 s2 = sm;
             }
+
+            //用来存放最终需要输出的字符串
             StringBuilder str = new StringBuilder();
+
+            //外层遍历较短的字符串，可以保证题目要求的如果长度相同输出较短的第一次出现的
             for (int i = 0; i < s1.length(); i++) {
-                
+
                 for (int j = 0; j < s2.length(); j++) {
                     if (s1.charAt(i) == s2.charAt(j)) {
+
+                        //暂时存放相同的字符串
                         StringBuilder s = new StringBuilder();
                         int s1L = i;
                         int s2L = j;
-                        
+
                         while (s1L < s1.length() && s2L < s2.length()) {
                             if (s1.charAt(s1L) == s2.charAt(s2L)) {
                                 s.append(s1.charAt(s1L));
@@ -31,14 +37,18 @@ public class Main{
                             s1L++;
                             s2L++;
                         }
+                        
+                        //如果新的字符串相同的长度比之前的长，就进行替换
                         if (s.length() > str.length()) {
                             str = s;
                         }
-                        
+
                     }
                 }
-                
+
             }
+            
+            //如果str的长度不为空，就代表有相同的字符串
             if (str.length() != 0) {
                 System.out.println(str.toString());
             }
