7c8f1332bb8479dcf5b78a5a43f789c4bc49e9c8
ShengHui <2933347352@qq.com>
2021-05-27 11:32:19 +0800

    注意n本身是素数的情况

---
+++ 0每日一题/5.22/因子个数/Main2.java
@@ -0,0 +1,33 @@
+// write your code here
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            HashSet<Integer> set = new HashSet<>();
+            int i = 2;
+            while (i < Math.sqrt(n)) {
+                while (n%i == 0) {
+                    set.add(i);
+                    n /= i;
+                }
+                i++;
+            }
+
+            /**
+             * ！！！！注意，如果最后的结果不是1 代表在比sqrt(n)大的数字里面还有一个是质因数，但是没有被算在set里面
+             * 比如26    sqrt(26) == 5.几
+             * i == 2开始计算       2 < 5.几    n = 13
+             * i == 3    3 < 3.几
+             * i == 4    4 < 3.几不成立，  但是set里面没有存储13这个值
+             */
+            if (n !=1) {
+                System.out.println(set.size()+1);
+            } else {
+                System.out.println(set.size());
+            }
+        }
+    }
+}
