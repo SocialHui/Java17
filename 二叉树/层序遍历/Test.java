709b448fcb3bd6457480281ebd6a2e35478848a8
ShengHui <2933347352@qq.com>
2021-07-04 10:06:01 +0800

    二叉树的层序遍历

---
+++ 二叉树/层序遍历/Test.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        List<Integer> list1 = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();  //每一行的数字有多少个！！！！！！！
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                list1.add(node.val);
+            }
+            list.add(list1);
+            list1 = new ArrayList<>();
+        }
+        return list;
+    }
+}
