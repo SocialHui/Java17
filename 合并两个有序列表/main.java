a3c6ad716a1e76812799ddba6830c5b334844b73
SocialHui <2933347352@qq.com>
2021-08-30 08:14:09 +0800

    第二次提交

---
+++ 合并两个有序列表/main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+/*
+ * public class ListNode {
+ *   int val;
+ *   ListNode next = null;
+ * }
+ */
+
+public class Solution {
+    /**
+     * 
+     * @param l1 ListNode类 
+     * @param l2 ListNode类 
+     * @return ListNode类
+     */
+    public ListNode mergeTwoLists (ListNode l1, ListNode l2) {
+        // write code here
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return head.next;
+    }
+}
