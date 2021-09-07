3c349346f744466cc9580d61b550ea8686873c5e
SocialHui <2933347352@qq.com>
2021-09-07 08:57:52 +0800

    1

---
+++ 二叉树/层序遍历/main.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+/*
+ * public class TreeNode {
+ *   int val = 0;
+ *   TreeNode left = null;
+ *   TreeNode right = null;
+ * }
+ */
+
+public class Solution {
+    /**
+     * 
+     * @param root TreeNode类 
+     * @return int整型ArrayList<ArrayList<>>
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder (TreeNode root) {
+        // write code here
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int num = queue.size();
+            ArrayList<Integer> list1 = new ArrayList<>();
+            while (num != 0) {
+                TreeNode tmp = queue.poll();
+                if (tmp.left != null) {
+                    queue.add(tmp.left);
+                }
+                if (tmp.right != null) {
+                    queue.add(tmp.right);
+                }
+                list1.add(tmp.val);
+                num--;
+            }
+            list.add(list1);
+        }
+        return list;
+    }
+}
